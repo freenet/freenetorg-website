a8a762ed7d018ee68554d351318b210a08cdfb95
Derek Briggs <contact@derekbriggs.xyz>
2022-04-21 22:59:36 -0400

    Configure Stripe PaymentElement

--- src/main/kotlin/xyz/derekbriggs/MainPage.kt
+++ src/main/kotlin/xyz/derekbriggs/MainPage.kt
@@ -1,15 +1,25 @@
 package xyz.derekbriggs
 
 import com.stripe.Stripe
+import kotlinx.serialization.json.JsonPrimitive
 import kweb.*
 import kweb.plugins.fomanticUI.fomantic
 import kweb.plugins.fomanticUI.fomanticUIPlugin
+import kweb.plugins.staticFiles.ResourceFolder
+import kweb.plugins.staticFiles.StaticFilesPlugin
 
 fun main() {
 
     Stripe.apiKey = "sk_test_51KYyh3HSi8gwBwE3syIyEQK1jd1HAJfWftPyWOspGL4cP0xfUz8RWfpHiRUGEjaIoKHBojzNvJQ6E7t3pb6E1l8l0032ZZFgK7"
 
-    Kweb(port = 8080, debug = false, plugins = listOf(fomanticUIPlugin, StripeRoutePlugin())) {
+    Kweb(port = 8080, debug = false, plugins = listOf(fomanticUIPlugin, StripeRoutePlugin(),
+        StaticFilesPlugin(ResourceFolder("static"), "/static/stripeCheckout"))) {
+        doc.head {
+            element("meta").setAttribute("content", "width=device-width, initial-scale=1").setAttribute("name", "viewport")
+            element("link").setAttribute("rel", "stylesheet").setAttribute("href", "/static/stripeCheckout/checkout.css")
+            element("script").setAttribute("src", "https://js.stripe.com/v3/")
+
+        }
         doc.body {
             route {
 
@@ -24,7 +34,6 @@ fun main() {
                 path("/") {
 
                     titleBar()
-
                     div(fomantic.pusher) {
                         div(fomantic.ui.inverted.vertical.masthead.center.aligned.segment) {
                             div(fomantic.ui.container) {
@@ -47,9 +56,17 @@ fun main() {
                                     val priceInput = input(type = InputType.text, placeholder = "5.00")
                                 }
                                 br()
-                                a() {
-                                    button(fomantic.ui.button).text("Reserve")
-                                }.setAttribute("href", "/checkout/")
+                                button(fomantic.ui.button).text("Reserve")
+                            }
+
+                            form(mapOf("id" to JsonPrimitive("payment-form"))) {
+                                div(mapOf("id" to JsonPrimitive("payment-element")))
+                                button(mapOf("id" to JsonPrimitive("submit"))) {
+                                    div(mapOf("id" to JsonPrimitive("spinner"))).classes("spinner hidden")
+                                    span(mapOf("id" to JsonPrimitive("button-text"))).text("Pay Now")
+                                    element("script").setAttribute("src", "/static/stripeCheckout/checkout.js")
+                                }
+                                div(mapOf("id" to JsonPrimitive("payment-message"))).classes("hidden")
                             }
                         }.setAttribute("style", """min-height: 700px;""")
                     }
--- src/main/kotlin/xyz/derekbriggs/StripeRoutePlugin.kt
+++ src/main/kotlin/xyz/derekbriggs/StripeRoutePlugin.kt
@@ -1,62 +1,53 @@
 package xyz.derekbriggs
 
+import com.google.gson.Gson
+import com.google.gson.annotations.SerializedName
+import com.stripe.Stripe
+import com.stripe.model.PaymentIntent
 import com.stripe.model.checkout.Session
-import com.stripe.param.InvoiceItemCreateParams
+import com.stripe.param.PaymentIntentCreateParams
 import com.stripe.param.checkout.SessionCreateParams
 import io.ktor.application.*
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import kweb.plugins.KwebPlugin
 
 class StripeRoutePlugin : KwebPlugin() {
 
+    companion object CreatePayment {
+        @SerializedName("items")
+        var items: Array<Any> = emptyArray()
+    }
+
+    internal class CreatePaymentResponse(private val clientSecret: String)
+
     override fun appServerConfigurator(routeHandler: Routing) {
-        println("appserver was configurated")
         routeHandler.get("/failing") {
             call.respondText("Failing")
         }
-        routeHandler.get("/checkout/") {
-            val YOUR_DOMAIN = "http://0.0.0.0:1234"
-            val sessionParams : SessionCreateParams =
-                SessionCreateParams.builder()
-                    .setMode(SessionCreateParams.Mode.PAYMENT)
-                    .setSuccessUrl(YOUR_DOMAIN + "/success")
-                    .setCancelUrl(YOUR_DOMAIN + "/cancel")
-                    .addLineItem(
-                        SessionCreateParams.LineItem.builder()
-                            .setPriceData(
-                                SessionCreateParams.LineItem.PriceData.builder()
-                                    .setProduct("donation_id")
-                                    .setUnitAmount(5000L)
-                                    .setCurrency("usd")
-                                    .build()
-                            )
-                            .setQuantity(1L)
-                            .build()
-                    )
-                    .build()
-            val session : Session = Session.create(sessionParams)
-            call.respondRedirect(session.url)
+        routeHandler.post("/create-payment-intent") {
+            Stripe.apiKey = "sk_test_51KYyh3HSi8gwBwE3syIyEQK1jd1HAJfWftPyWOspGL4cP0xfUz8RWfpHiRUGEjaIoKHBojzNvJQ6E7t3pb6E1l8l0032ZZFgK7"
+            val gson = Gson()
+            val requestBody = call.receiveText()
+            val postBody : CreatePayment = gson.fromJson(requestBody, CreatePayment::class.java)
+            println("receiveText = $requestBody")
+            println(postBody)
+            val params : PaymentIntentCreateParams = PaymentIntentCreateParams.builder()
+                .setAmount(5000L)
+                .setCurrency("USD")
+                .setAutomaticPaymentMethods(
+                    PaymentIntentCreateParams.AutomaticPaymentMethods
+                        .builder()
+                        .setEnabled(true)
+                        .build()
+                ).build()
+
+            val paymentIntent = PaymentIntent.create(params)
+
+            val paymentResponse = CreatePaymentResponse(paymentIntent.clientSecret)
+            call.respond(gson.toJson(paymentResponse))
         }
-        /*routeHandler.post("/create-checkout-session") {
-            println("routehandler Ran")
-            val YOUR_DOMAIN = "http://0.0.0.0:1234"
-            val sessionParams : SessionCreateParams =
-                SessionCreateParams.builder()
-                    .setMode(SessionCreateParams.Mode.PAYMENT)
-                    .setSuccessUrl(YOUR_DOMAIN + "/success")
-                    .setCancelUrl(YOUR_DOMAIN + "/cancel")
-                    .addLineItem(
-                        SessionCreateParams.LineItem.builder()
-                            .setQuantity(1L)
-                            .setPrice("donation_price_2")
-                            .build()
-                    )
-                    .build()
-            val session : Session = Session.create(sessionParams)
-            call.respondRedirect(session.url)
-            //call.response
-        }*/
         super.appServerConfigurator(routeHandler)
     }
 }
