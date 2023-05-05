1ae2f3a120dd3280d8dff9d4f34823c8db01a4f2
Ian Clarke <ian.clarke@gmail.com>
2023-05-05 14:50:37 -0500

    copy improvements

--- src/main/kotlin/org/freenet/website/pages/news/latestNews.kt
+++ src/main/kotlin/org/freenet/website/pages/news/latestNews.kt
@@ -2,13 +2,15 @@ package org.freenet.website.pages.news
 
 import kweb.components.Component
 import kweb.div
+import kweb.h2
 import kweb.h3
 import kweb.plugins.fomanticUI.fomantic
 import kweb.state.ObservableList
 import kweb.state.renderEach
 
 fun Component.latestNews(newsItems: ObservableList<NewsItem>) {
-    h3().classes("title").text("Latest News")
+    h3().text("Latest News")
+
     div(fomantic.ui.bulleted.list) {
         renderEach(newsItems) { newsItem ->
             div(fomantic.item) {
---
+++ src/main/kotlin/org/freenet/website/util/components.kt
@@ -0,0 +1,26 @@
+package org.freenet.website.util
+
+import kweb.a
+import kweb.components.Component
+import kweb.i
+import kweb.span
+
+fun Component.iconButton(html : String, href : String, icon : Array<String>, buttonClasses : Array<String> = arrayOf("button")) {
+    a { a ->
+        a.classes(*buttonClasses)
+        a.href = href
+
+        span { span ->
+            span.classes("icon")
+
+            i { i ->
+                i.classes(*icon)
+            }
+        }
+
+        span { span ->
+            span.innerHTML(html)
+        }
+
+    }
+}
