a4757e4b881087e8c26b497a69b622b80d327b66
Ian Clarke <ian.clarke@gmail.com>
2023-05-05 17:50:40 -0500

    copy improvements

--- src/main/kotlin/org/freenet/website/navigationBar.kt
+++ src/main/kotlin/org/freenet/website/navigationBar.kt
@@ -17,13 +17,13 @@ fun Component.navComponent(activeItem: KVal<NavItem>) {
                     a.classes(activeItem.map { if (it == ni) "navbar-item is-active" else "navbar-item" })
                     if (ni.icon != null) {
                         span { span ->
-                            span.classes("icon", "is-small")
+                            span.classes("icon",) // "is-small")
                             i { i ->
                                 i.classes("fas", "fa-${ni.icon}")
                             }
                         }
                         // Add a space between the icon and the text
-                        span().innerHTML("&nbsp;&nbsp;")
+                        span().innerHTML("&nbsp;")
                         span().innerHTML(ni.html)
                     } else {
                         a.innerHTML(ni.html)
