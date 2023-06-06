3c302eec5c7433adca90a330320df4d4152cfe7a
Ian Clarke <ian.clarke@gmail.com>
2023-06-06 14:39:09 -0500

    don't use KVar for faqHtml as it seemed to accelerate a memory leak

--- src/main/kotlin/org/freenet/website/pages/faq/faqPage.kt
+++ src/main/kotlin/org/freenet/website/pages/faq/faqPage.kt
@@ -6,36 +6,34 @@ import kweb.state.render
 import org.freenet.website.util.retrievePage
 import org.jsoup.Jsoup
 
-// TODO: Move code below to shared KVar
-// TODO:
-private val faqHtml = retrievePage("https://raw.githubusercontent.com/wiki/freenet/locutus/Frequently-Asked-Questions.md")
+private val faqHtml =
+    retrievePage("https://raw.githubusercontent.com/wiki/freenet/locutus/Frequently-Asked-Questions.md")
 
 fun Component.faqPage() {
-    render(faqHtml) { faqHtml ->
-        if (faqHtml == null) {
-            div().text("Loading FAQ...")
-        } else {
-            val jsoupDoc = Jsoup.parse(faqHtml)
-            var faqNumber = 0
-            val headings = ArrayList<String>()
-            jsoupDoc.select("h1").forEach { h1 ->
-                h1.id("faq-$faqNumber")
-                headings += h1.html()
-                faqNumber++
-            }
-            h1().text("Frequently Asked Questions")
-            ul {
-                for ((ix, heading) in headings.withIndex()) {
-                    li {
-                        a { a ->
-                            a["href"] = "#faq-$ix"
+    val faqHtml = faqHtml.value
+    if (faqHtml == null) {
+        div().text("Loading FAQ...")
+    } else {
+        val jsoupDoc = Jsoup.parse(faqHtml)
+        var faqNumber = 0
+        val headings = ArrayList<String>()
+        jsoupDoc.select("h1").forEach { h1 ->
+            h1.id("faq-$faqNumber")
+            headings += h1.html()
+            faqNumber++
+        }
+        h1().text("Frequently Asked Questions")
+        ul {
+            for ((ix, heading) in headings.withIndex()) {
+                li {
+                    a { a ->
+                        a["href"] = "#faq-$ix"
 
-                            a.innerHTML(heading)
-                        }
+                        a.innerHTML(heading)
                     }
                 }
             }
-            div().innerHTML(jsoupDoc.body().html())
         }
+        div().innerHTML(jsoupDoc.body().html())
     }
 }
