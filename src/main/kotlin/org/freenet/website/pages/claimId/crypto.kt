22a9a9919eb289dd519760ff7cd27cf6d50230a8
Derek Briggs <contact@derekbriggs.xyz>
2023-10-30 22:04:48 -0400

    Complete the functionality of crypto.kt

--- src/main/kotlin/org/freenet/website/pages/claimId/crypto.kt
+++ src/main/kotlin/org/freenet/website/pages/claimId/crypto.kt
@@ -1,28 +1,14 @@
 package org.freenet.website.pages.claimId
 
 import java.security.KeyFactory
+import java.security.KeyPair
 import java.util.Base64
 import java.security.KeyPairGenerator
+import java.security.Signature
 import java.security.interfaces.RSAPrivateKey
 import java.security.interfaces.RSAPublicKey
 import java.security.spec.PKCS8EncodedKeySpec
 import java.security.spec.X509EncodedKeySpec
-
-object IdCrypto {
-
-}
-
-fun RSAPrivateKey.toPEM(): String {
-    val privateKeyPEM = StringBuilder()
-    val encoder = Base64.getEncoder()
-
-    // Wrap the private key in the appropriate PEM header and footer
-    privateKeyPEM.append("-----BEGIN PRIVATE KEY-----\n")
-    privateKeyPEM.append(encoder.encodeToString(this.encoded))
-    privateKeyPEM.append("\n-----END PRIVATE KEY-----\n")
-
-    return privateKeyPEM.toString()
-}
 
 fun RSAPublicKey.encodeToPem(): String {
     val publicKeyPEM = StringBuilder()
@@ -36,44 +22,78 @@ fun RSAPublicKey.encodeToPem(): String {
     return publicKeyPEM.toString()
 }
 
-fun toFreenetRSAPublicKey(pem : String): RSAPublicKey {
-    if (!pem.contains("-----BEGIN PUBLIC KEY-----") || !pem.contains("-----END PUBLIC KEY-----")) {
-        throw IllegalArgumentException("String does not contain valid public key PEM")
+class RSASigner {
+
+    object FreenetKey {
+        lateinit var freenetRSAKeyPair: KeyPair
+        lateinit var freenetRSAPublicKeyPEM: String
+
+        fun initialize() {
+            // Generate a key pair
+            val keyPairGenerator = KeyPairGenerator.getInstance("RSA")
+            keyPairGenerator.initialize(2048)
+            freenetRSAKeyPair = keyPairGenerator.genKeyPair()
+
+            freenetRSAPublicKeyPEM = (freenetRSAKeyPair.public as RSAPublicKey).encodeToPem()
+            println(freenetRSAPublicKeyPEM)
+        }
     }
 
-    val publicKeyPEM = pem
-        .replace("-----BEGIN PUBLIC KEY-----", "")
-        .replace("-----END PUBLIC KEY-----", "")
-        .replace("\n", "")
+    fun RSAPrivateKey.toPEM(): String {
+        val privateKeyPEM = StringBuilder()
+        val encoder = Base64.getEncoder()
 
-    val keySpecX509 = X509EncodedKeySpec(Base64.getDecoder().decode(publicKeyPEM))
-    val kf = KeyFactory.getInstance("RSA")
-    return kf.generatePublic(keySpecX509) as RSAPublicKey
-}
+        // Wrap the private key in the appropriate PEM header and footer
+        privateKeyPEM.append("-----BEGIN PRIVATE KEY-----\n")
+        privateKeyPEM.append(encoder.encodeToString(this.encoded))
+        privateKeyPEM.append("\n-----END PRIVATE KEY-----\n")
 
-fun toFreenetRSAPrivateKey(pem : String): RSAPrivateKey {
-    if (!pem.contains("-----BEGIN PRIVATE KEY-----") || !pem.contains("-----END PRIVATE KEY-----")) {
-        throw IllegalArgumentException("String does not contain valid private key PEM")
+        return privateKeyPEM.toString()
     }
 
-    val privateKeyPEM = pem
-        .replace("-----BEGIN PRIVATE KEY-----", "")
-        .replace("-----END PRIVATE KEY-----", "")
-        .replace("\n", "")
 
-    val keySpecPKCS8 = PKCS8EncodedKeySpec(Base64.getDecoder().decode(privateKeyPEM))
-    val kf = KeyFactory.getInstance("RSA")
-    return kf.generatePrivate(keySpecPKCS8) as RSAPrivateKey
+
+    fun toFreenetRSAPublicKey(pem: String): RSAPublicKey {
+        if (!pem.contains("-----BEGIN PUBLIC KEY-----") || !pem.contains("-----END PUBLIC KEY-----")) {
+            throw IllegalArgumentException("String does not contain valid public key PEM")
+        }
+
+        val publicKeyPEM = pem
+            .replace("-----BEGIN PUBLIC KEY-----", "")
+            .replace("-----END PUBLIC KEY-----", "")
+            .replace("\n", "")
+
+        val keySpecX509 = X509EncodedKeySpec(Base64.getDecoder().decode(publicKeyPEM))
+        val kf = KeyFactory.getInstance("RSA")
+        return kf.generatePublic(keySpecX509) as RSAPublicKey
+    }
+
+    fun toFreenetRSAPrivateKey(pem: String): RSAPrivateKey {
+        if (!pem.contains("-----BEGIN PRIVATE KEY-----") || !pem.contains("-----END PRIVATE KEY-----")) {
+            throw IllegalArgumentException("String does not contain valid private key PEM")
+        }
+
+        val privateKeyPEM = pem
+            .replace("-----BEGIN PRIVATE KEY-----", "")
+            .replace("-----END PRIVATE KEY-----", "")
+            .replace("\n", "")
+
+        val keySpecPKCS8 = PKCS8EncodedKeySpec(Base64.getDecoder().decode(privateKeyPEM))
+        val kf = KeyFactory.getInstance("RSA")
+        return kf.generatePrivate(keySpecPKCS8) as RSAPrivateKey
+    }
+
+    //Performs an RSA blind signature on the client's blinded key
+    fun RSASign(message: String): String {
+        val privateSignature = Signature.getInstance("SHA256withRSA")
+        privateSignature.initSign(FreenetKey.freenetRSAKeyPair.private)
+
+        privateSignature.update(message.toByteArray())
+
+        val signature = privateSignature.sign()
+
+        val signatureString = Base64.getEncoder().encodeToString(signature)
+        return signatureString
+    }
+
 }
-
-fun main() {
-    // Generate a key pair
-    val keyPairGenerator = KeyPairGenerator.getInstance("RSA")
-    keyPairGenerator.initialize(2048)
-    val freenetRSAKeyPair = keyPairGenerator.genKeyPair()
-
-    // Serialize the public key to a PEM string
-    val freenetRSAPublicKeyPEM = (freenetRSAKeyPair.public as RSAPublicKey).encodeToPem()
-    println(freenetRSAPublicKeyPEM)
-}
-
