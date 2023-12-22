a4ac7ed43136c6bc6c5be4dfadb24e3b8f100151
Derek Briggs <contact@derekbriggs.xyz>
2023-12-22 00:26:45 -0500

    Delete unneeded pages

--- src/main/kotlin/org/freenet/website/pages/claimId/contributionPage.kt
+++
@@ -1,7 +0,0 @@
-package org.freenet.website.pages.claimId
-
-import kweb.components.Component
-
-fun Component.contributionPage() {
-
-}
--- src/main/kotlin/org/freenet/website/pages/claimId/successfulPayPage.kt
+++
@@ -1,8 +0,0 @@
-package org.freenet.website.pages.claimId
-
-import kweb.components.Component
-import kweb.h1
-
-fun Component.successfulPayPage() {
-    h1().text("Hola")
-}
